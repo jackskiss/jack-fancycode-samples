8cc297eb4bcdc145ffbe32bc368b82fc2b730a68
Jack Min <jack.min@obigo.com>
2014-03-13 13:06:24 +0900

    Add Unix Domain Socket Function

--- socket/client/socket_client.c
+++ socket/client/socket_client.c
@@ -1,4 +1,4 @@
-nclude <sys/socket.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -8,13 +8,24 @@ nclude <sys/socket.h>
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h> 
+#include <sys/un.h>
+
+#define ENABLE_UNIX_DOMAIN_SOCKET
+
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET
+const char * file_server = "/tmp/udstest";
+#endif
+
 
 int main(int argc, char *argv[])
 {
     int sockfd = 0, n = 0;
     char recvBuff[1024];
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET    
+    struct sockaddr_un serv_addr; 
+#else
     struct sockaddr_in serv_addr; 
-
+#endif
     if(argc != 2)
     {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
@@ -22,7 +33,11 @@ int main(int argc, char *argv[])
     } 
 
     memset(recvBuff, '0',sizeof(recvBuff));
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET  
+    if((sockfd = socket(PF_FILE, SOCK_STREAM, 0)) < 0)
+#else
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+#endif
     {
         printf("\n Error : Could not create socket \n");
         return 1;
@@ -30,20 +45,29 @@ int main(int argc, char *argv[])
 
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET    
+    serv_addr.sun_family = AF_UNIX;
+    strcpy(serv_addr.sun_path, file_server);
+#else
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
+#endif
 
+#ifndef ENABLE_UNIX_DOMAIN_SOCKET        
     if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
     } 
+#endif
 
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Error : Connect Failed \n");
        return 1;
     } 
+
+    write(sockfd, "Hello Server", 13);
 
     while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
     {
--- socket/server/socket_server.c
+++ socket/server/socket_server.c
@@ -8,22 +8,46 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h> 
+#include <sys/un.h>
+
+
+#define ENABLE_UNIX_DOMAIN_SOCKET
+
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET
+const char * file_server = "/tmp/udstest";
+#endif
 
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET    
+    struct sockaddr_un serv_addr; 
+#else
     struct sockaddr_in serv_addr; 
-
+#endif
     char sendBuff[1025];
-    time_t ticks; 
+    time_t ticks;
 
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET
+    listenfd = socket(PF_FILE, SOCK_STREAM, 0);
+#else
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
+#endif
+
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff)); 
 
-    serv_addr.sin_family = AF_INET;
+#ifndef ENABLE_UNIX_DOMAIN_SOCKET        
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+#endif
+    
+#ifdef ENABLE_UNIX_DOMAIN_SOCKET
+    serv_addr.sun_family = AF_UNIX;
+    strcpy(serv_addr.sun_path, file_server);
+#else
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
+#endif
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
@@ -32,6 +56,10 @@ int main(int argc, char *argv[])
     while(1)
     {
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+
+        read(connfd, sendBuff, sizeof(sendBuff));
+        
+        printf("%s\n", sendBuff);
 
         ticks = time(NULL);
         snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
