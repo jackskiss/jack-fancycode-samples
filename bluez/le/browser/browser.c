6175b0effdfcd87ddfb5c30b60dba12a75e06080
Jack Min <jack.ko.min@gmail.com>
2015-06-27 21:43:54 +0900

    Fix compile error

--- bluez/le/browser/browser.c
+++ bluez/le/browser/browser.c
@@ -14,6 +14,11 @@ int main(int argc, char **argv)
 {
 	inquiry_info *ii = NULL;
 	int dev_id, sock;
+	int len, i;
+	int num_rsp, max_rsp, flags;
+
+	char addr[19] = { 0 };
+	char name[248] = { 0 };
 
 	dev_id = hci_get_route(NULL);
 	
@@ -21,8 +26,33 @@ int main(int argc, char **argv)
 	
 	if (dev_id < 0 || sock < 0) {
 		//error
+		printf("Error dev id : %d sock : %d\n", dev_id, sock);
 		exit(1);
 	}
+
+	printf("Result dev id : %d sock : %d\n", dev_id, sock);
+
+    len  = 8;
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+    
+    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+    if( num_rsp < 0 ) perror("hci_inquiry");
+
+    printf ("num_rsp : %d\n", num_rsp);
+    
+    for (i = 0; i < num_rsp; i++) {
+        ba2str(&(ii+i)->bdaddr, addr);
+        memset(name, 0, sizeof(name));
+        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
+            name, 0) < 0)
+        strcpy(name, "[unknown]");
+        printf("%s  %s\n", addr, name);
+    }
+
+    free( ii );
+    close( sock );
 
 	return 0;
 }
