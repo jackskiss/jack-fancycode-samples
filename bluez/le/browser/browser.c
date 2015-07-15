d93a7f298002f8e8fbbad57b78c3142a7db358fe
Jack Min <jack.ko.min@gmail.com>
2015-07-16 07:46:27 +0900

    Add status of bluez

--- bluez/le/browser/browser.c
+++ bluez/le/browser/browser.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <dirent.h>
+#include <errno.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
@@ -28,8 +29,9 @@ static int hci_count()
 
 	while((entry = readdir(dirp)) != NULL)
 	{
-		if (entry->d_type == DT_DIR) { /* directory type is not unknown */
+		if (entry->d_type == DT_LNK) { /* directory type is not unknown */
 //		if (entry->d_type != DT_UNKNOWN) { /* directory type is not unknown */
+			printf("%s type is %d\n", entry->d_name, entry->d_type);
 			count++;
 		}
 	}
@@ -37,6 +39,58 @@ static int hci_count()
 	closedir(dirp);
 
 	return count;
+}
+
+static void hci_info(int devid)
+{
+	int ret;
+	struct hci_dev_info info;
+
+	ret = hci_dev_info (devid, &info);
+
+/*	struct hci_dev_info {
+		uint16_t dev_id;
+		char     name[8];
+
+		bdaddr_t bdaddr;
+
+		uint32_t flags;
+		uint8_t  type;
+
+		uint8_t  features[8];
+
+		uint32_t pkt_type;
+		uint32_t link_policy;
+		uint32_t link_mode;
+
+		uint16_t acl_mtu;
+		uint16_t acl_pkts;
+		uint16_t sco_mtu;
+		uint16_t sco_pkts;
+
+		struct   hci_dev_stats stat;
+	};	
+*/
+}
+
+static void cmd_scan(int ctl, int hdev, char *opt)
+{
+	struct hci_dev_req dr;
+
+	dr.dev_id  = hdev;
+	dr.dev_opt = SCAN_DISABLED;
+	if (!strcmp(opt, "iscan"))
+		dr.dev_opt = SCAN_INQUIRY;
+	else if (!strcmp(opt, "pscan"))
+		dr.dev_opt = SCAN_PAGE;
+	else if (!strcmp(opt, "piscan"))
+		dr.dev_opt = SCAN_PAGE | SCAN_INQUIRY;
+
+	if (ioctl(ctl, HCISETSCAN, (unsigned long) &dr) < 0) {
+		fprintf(stderr, "Can't set scan mode on hci%d: %s (%d)\n",
+						hdev, strerror(errno), errno);
+		exit(1);
+	}
 }
 
 int main(int argc, char **argv)
@@ -59,7 +113,7 @@ int main(int argc, char **argv)
 	num_hci = hci_count();
 
 //	dev_id = hci_get_route(NULL);
-	dev_id = hci_get_route(&leadd_0);
+	dev_id = hci_get_route(&leadd_1);
 
 	printf("device id : %d\n", dev_id);
 
@@ -85,6 +139,9 @@ int main(int argc, char **argv)
 	for (dev_id = 0 ; dev_id < num_hci; dev_id++)
 	{
 		sock = hci_open_dev(dev_id);
+
+		cmd_scan(sock, dev_id, "pscan");
+
 	    len  = 8;
 	    max_rsp = 255;
 	    flags = IREQ_CACHE_FLUSH;
@@ -98,6 +155,7 @@ int main(int argc, char **argv)
 	    for (i = 0; i < num_rsp; i++) {
 	        ba2str(&(ii+i)->bdaddr, addr);
 	        memset(name, 0, sizeof(name));
+	        /* Display scanned device list */
 	        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
 	            name, 0) < 0)
 	        strcpy(name, "[unknown]");
@@ -106,6 +164,7 @@ int main(int argc, char **argv)
 
 	    free( ii );
 	    close( sock );
+	    hci_close_dev(dev_id);
 	}
 
 	return 0;
